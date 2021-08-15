df082d1cc7b8cb5fbe94dc2890e7a11b5758a58e
Scott Seligman <scott.seligman@gmail.com>
2021-08-14 21:47:26 -0700

    Basic charts

---
+++ README.template.md
@@ -0,0 +1,7 @@
+# Track IPs for Cloud Providers
+
+Work in progress to track the relative size, in terms of IP addresses, for different cloud providers.
+
+![Compared](images/main.png)
+
+[[history]]
--- update_data.py
+++ update_data.py
@@ -2,6 +2,9 @@
 
 import subprocess
 from datetime import datetime
+import os
+import matplotlib.pyplot as plt
+import json
 
 started = datetime.utcnow()
 def log_step(value):
@@ -17,6 +20,55 @@ log_step("Starting work")
 
 # Main worker
 run(["python3", "get_all.py"])
+
+# Draw the images
+if not os.path.isdir("images"):
+    os.mkdir("images")
+
+data = []
+with open("summary.jsonl", "rt") as f:
+    for row in f:
+        data.append(row)
+        if len(data) > 365:
+            data.pop(0)
+
+providers = set()
+for i in range(len(data)):
+    data[i] = json.loads(data[i])
+    providers |= set(data[i])
+
+md = ""
+providers.remove("_")
+with plt.style.context("dark_background"):
+    log_step("Main chart")
+    plt.figure(figsize=(7, 5))
+    plt.bar(range(len(providers)), [data[-1].get(x, [0])[0] for x in sorted(providers)])
+    plt.xticks(range(len(providers)), sorted(providers))
+    plt.yticks(plt.yticks()[0], [f"{int(x/1000000):d}m" for x in plt.yticks()[0]])
+    plt.tight_layout()
+    plt.savefig(os.path.join("images", "main.png"), dpi=100)
+
+    for cur in providers:
+        log_step(f"Chart for {cur}")
+        md += f"![{cur}](images/history_{cur}.png)\n\n"
+        plt.clf()
+        plt.figure(figsize=(7, 2))
+        history = [x.get(cur, [0])[0] for x in data]
+        plt.plot(range(len(history)), history)
+        plt.xticks([])
+        plt.yticks([])
+        plt.ylabel("gcloud")
+        plt.tight_layout()
+        plt.savefig(os.path.join("images", f"history_{cur}.png"), dpi=100)
+
+# Update the README
+log_step("README")
+with open("README.template.md", "rt") as f_src:
+    with open("README.md", "wt") as f_dest:
+        data = f_src.read()
+        data = data.replace("[[history]]", md)
+
+        f_dest.write(data)
 
 # Check in any changes
 run(["git", "add", "."])
