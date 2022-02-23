6b7bbb201b6cd7b052e26de9e82ee7682dec66ab
Scott Seligman <scott.seligman@gmail.com>
2022-02-23 12:34:55 -0800

    Add historical data

---
+++ history/README.md
@@ -0,0 +1,7 @@
+# Cloud Sizes History
+
+This directory includes a history of the data files from the different cloud providers.
+
+Technically it's possible to pull this history from Git.  Hopefully a second copy here makes it easy to parse if you want to run any scripts on the historical data.  The layout should be fairly self-explanatory.
+
+The Python script here, `pull_out_history.py`, will run on a clone of this repo and create individual files from the git history for this repo of the data files for the year in progress that's not yet checked into this folder.
---
+++ history/pull_out_history.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import subprocess
+from hashlib import sha256
+import json
+from datetime import datetime
+import os
+import gzip
+
+"""
+YEAR=2021 ; find ${YEAR} -type f | sort | tar --owner=0 --group=0 -T - -cvzf ${YEAR}.tar.gz
+"""
+
+def process_history(fn, provider):
+    seen = set()
+
+    cmd = "git rev-list --all --objects -- ../data/" + fn
+    history = subprocess.check_output(cmd.split(' ')).decode("utf8")
+    for row in history.split("\n"):
+        row = row.strip().split(' ')
+        if len(row) == 2 and row[1] == "data/" + fn:
+            print("Working on " + provider + ":" + row[0], end="", flush=True)
+            cmd = "git cat-file -p " + row[0]
+            data = subprocess.check_output(cmd.split(' '))
+            data = gzip.decompress(data)
+            hash = sha256(data).hexdigest()
+            if hash in seen:
+                print(", dupe detected", flush=True)
+            else:
+                seen.add(hash)
+                time = datetime(*[int(x) for x in json.loads(data)['date'][:19].replace(" ", "-").replace(":", "-").split("-")])
+                if os.path.isfile(time.strftime("%Y") + ".tar.gz"):
+                    print(", already archived " + time.strftime("%Y-%m-%d %H:%M:%S"), flush=True)
+                    break
+                else:
+                    dn = time.strftime("%Y")
+                    if not os.path.isdir(dn):
+                        os.mkdir(dn)
+                    dn = os.path.join(dn, provider)
+                    if not os.path.isdir(dn):
+                        os.mkdir(dn)
+                    with open(os.path.join(dn, time.strftime("%Y-%m-%d-%H-%M-%S.json")), "wb") as f:
+                        f.write(data)
+                    print(", created file for " + time.strftime("%Y-%m-%d %H:%M:%S"), flush=True)
+
+def main():
+    for cur in sorted(os.listdir(os.path.join("..", "data"))):
+        if cur.startswith("data_") and cur.endswith(".json.gz"):
+            provider = cur[5:-8]
+            process_history(cur, provider)
+
+if __name__ == "__main__":
+    main()
